9fb4f21570572e2e9bc27f0d03592cf0206594e0
roshan472 <roshanfrancis190@gmail.com>
2023-03-29 10:23:38 +0530

    changes

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,9 +8,9 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
+            Hi, I am <span className="purple">M F ROSHAN </span>
+            from <span className="purple"> Ernakulam,kerala.</span>
+            <br />I am pursuing BCA AT RAJAGIRI COLLEGE
             Mesra.
             <br />
             <br />
@@ -18,20 +18,15 @@ function AboutCard() {
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Hacking
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Singing
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
             </li>
           </ul>
-
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
-          </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
         </blockquote>
       </Card.Body>
     </Card>
